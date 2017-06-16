88f1852d65051ce0be6c813541ed5cad83d0fd9b
lumenghe <lumenghe@gmail.com>
2017-06-16 19:24:52 +0100

    first file commit, workflow modification

--- sudoku.py
+++ sudoku.py
@@ -19,3 +19,26 @@ def string_matrix(m):
 class Sudoku(object):
     def __init__(self, filename):
         self.matrix, self.max_value, self.block_size = self.read_instance(filename)
+
+    def read_instance(self, filename):
+        """
+        Load instance from filename to self.matrix, load 0 if it is an empty value
+        """
+        matrix = []
+        try:
+            with open(filename, 'r') as f:
+                for line in f:
+                    row = []
+                    for x in line.strip().split():
+                        row.append(0 if x=='_' else int(x))
+                    matrix.append(row)
+        except:
+            raise ValueError("could not load file {}".format(filename))
+        n = len(matrix)
+        if not n:
+            raise ValueError("Sodoku matrix is empty")
+        if not all(len(row) == n for row in matrix):
+            raise ValueError("Rows should have same size as columns")
+        if int(sqrt(n))**2 != n:
+            raise ValueError("Size of a sudoku should be a square number")
+        return matrix, n, int(sqrt(n))
